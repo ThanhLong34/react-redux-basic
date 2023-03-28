d101d8a1eb144343cf284ed1de5a5b819f9895ca
DragonDev <thanhlongedu0304@gmail.com>
2023-03-28 18:29:07 +0700

    Install redux, react-redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 function App() {
-  return (
-    <div className="App">
-      <h1>Vite + React</h1>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+	return (
+		<div className="App">
+			<h1>Vite + React</h1>
+			<p className="read-the-docs">
+				Click on the Vite and React logos to learn more
+			</p>
+		</div>
+	);
 }
 
-export default App
+export default App;
